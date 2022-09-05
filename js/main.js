cfdb86b2e424d8751a243bed5e39680dbf7e6439
Initech_ <107325812+A-Initech@users.noreply.github.com>
2022-09-05 14:19:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const users=[{id:1,nom:"jean",prenom:"pierre",age:25}];
+const validateButton= document.getElementById('valider');
+//
+validateButton.addEventListener('click',addUser);
+
+function addUser(e) {
+
+    //Fonctionalité
+
+e.preventDefault();
+    const enteredUserData={}
+}
